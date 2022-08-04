440d70dee607beb771703e78882ae194dbaf68df
Owen <you@example.comowen.d.hearn@gmail.com>
2022-08-04 15:31:31 +0200

    Laptop push

--- main_linkedlist.c
+++ main_linkedlist.c
@@ -6,7 +6,7 @@
 /*   By: owen <owen@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/28 19:36:08 by owen          #+#    #+#                 */
-/*   Updated: 2022/08/01 15:05:05 by ohearn        ########   odam.nl         */
+/*   Updated: 2022/08/01 16:21:13 by owen          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,26 @@ int main()
 	t_list *laster;
 	t_list **placeholder;
 
-	list_start = ft_lstnew((void *)42);
-	second = ft_lstnew((void *)24);
-	last = ft_lstnew((void *)68);
+	list_start = ft_lstnew((void *)"42");
+	second = ft_lstnew((void *)"24");
+	last = ft_lstnew((void *)"68");
 	temp = list_start;
 	list_start->next = second;
 	second->next = last;
 	last->next = NULL;
 	while (temp != NULL)
 	{
-		printf("%d\n",temp->content);
+		printf("%s\n",temp->content);
 		temp = temp->next;
 	}
 	printf("\n");
 	placeholder = &list_start;
-	laster = ft_lstnew((void*)45536);
+	laster = ft_lstnew((void*)"This works as well now");
 	ft_lstadd_back(&list_start, laster);
 	temp = list_start;
 	while (temp != NULL)
 	{
-		printf("%d\n",temp->content);
+		printf("%s\n",temp->content);
 		temp = temp->next;
 	}
 }
