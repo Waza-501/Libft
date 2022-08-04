b702c8c1fbf813ef76555bce9450fbbc2c6daffd
Owen Hearn <ohearn@f1r4s16.codam.nl>
2022-08-04 20:02:17 +0200

    write split main before handing it in

---
+++ main_split.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main_split.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ohearn <ohearn@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/04 15:40:10 by ohearn        #+#    #+#                 */
+/*   Updated: 2022/08/04 20:02:02 by ohearn        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <stdio.h>
+
+int	main()
+{
+	char **test;
+
+	test = ft_split("Hello this is a test to see if this works", " ");
+	while 
+}
