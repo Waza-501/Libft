fe0faf21c7db64e24f2126855c5f887b2c47539c
owhearn <owen.d.hearn@gmail.com>
2024-10-07 12:44:59 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,36 @@
+Library				=	libft
+Files				=	ft_bzero.c\
+						ft_isalnum.c\
+						ft_isalpha.c\
+						ft_isascii.c\
+						ft_isdigit.c\
+						ft_isprint.c\
+						ft_strlen.c\
+						ft_tolower.c\
+						ft_toupper.c\
+
+
+Compiler		=	gcc
+Cflags			=	-Wall -Werror -Wextra
+
+OUTN			= $(Library).a
+
+Ofiles			= $(Files:%.c=%.o)
+
+NAME			= $(OUTN)
+
+$(NAME):
+		$(Compiler) $(Cflags) -c $(Files) -I ./
+		ar -rc $(OUTN) $(Ofiles)
+
+all:	$(NAME)
+
+clean:    
+		rm -rf $(Ofiles)
+
+fclean:	clean
+		rm -rf $(NAME)
+
+re:			fclean all
+
+.PHONY:		all, clean, fclean, re
---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: owhearn <owhearn@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 11:00:05 by owhearn           #+#    #+#             */
+/*   Updated: 2024/10/07 12:19:29 by owhearn          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+
+#endif
